d8551d610e475cbf0bfb8eb9c86373120bc3ae97
michaelPotter <mpotter.dev@gmail.com>
2024-09-13 09:30:06 -0700

    Add some Buf methods

--- lua/Buf.lua
+++ lua/Buf.lua
@@ -52,6 +52,10 @@ end
 --                              METHODS                               --
 ------------------------------------------------------------------------
 
+--
+-- LINES
+--
+
 --- @param start number?
 --- @param end_ number?
 --- @param strict_indexing boolean?
@@ -59,6 +63,52 @@ function Buf:get_lines(start, end_, strict_indexing)
 	if strict_indexing == nil then strict_indexing = true end
 	return vim.api.nvim_buf_get_lines(self.bufnr, start or 0, end_ or -1, strict_indexing)
 end
+
+--- Sets (replaces) a line-range in the buffer.
+--- 
+--- Indexing is zero-based, end-exclusive. Negative indices are interpreted as
+--- length+1+index: -1 refers to the index past the end. So to change or
+--- delete the last element use start=-2 and end=-1.
+--- 
+--- To insert lines at a given index, set `start` and `end` to the same index.
+--- To delete a range of lines, set `replacement` to an empty array.
+--- 
+--- Out-of-bounds indices are clamped to the nearest valid value, unless
+--- `strict_indexing` is set.
+
+--- @param start number
+--- @param end_ number
+--- @param strict_indexing boolean
+--- @param replacement string[]
+--- @return Buf
+function Buf:set_lines(start, end_, strict_indexing, replacement)
+	if strict_indexing == nil then strict_indexing = true end
+	vim.api.nvim_buf_set_lines(self.bufnr, start, end_, strict_indexing, replacement)
+	return self
+end
+
+--
+-- TEXT
+--
+
+-- TODO add get_text
+
+function Buf:set_text(start_row, start_col, end_row, end_col, replacement)
+	vim.api.nvim_buf_set_text(self.bufnr, start_row, start_col, end_row, end_col, replacement)
+	return self
+end
+
+--
+-- LINE COUNT
+--
+
+function Buf:get_line_count()
+	return vim.api.nvim_buf_line_count(self.bufnr)
+end
+
+--
+-- NAME
+--
 
 function Buf:get_name()
 	return vim.api.nvim_buf_get_name(self.bufnr)
@@ -69,14 +119,39 @@ function Buf:set_name(name)
 	return vim.api.nvim_buf_set_name(self.bufnr, name)
 end
 
+--
+-- LOADED
+--
+
 function Buf:is_loaded()
 	return vim.api.nvim_buf_is_loaded(self.bufnr)
 end
+
+--
+-- OPTIONS
+--
+
+--- @param name string
+function Buf:get_option(name)
+	vim.api.nvim_get_option_value(name, {buf = self.bufnr})
+end
+
+--- @param name string
+--- @param value any
+function Buf:set_option(name, value)
+	vim.api.nvim_set_option_value(name, value, {buf = self.bufnr})
+	return self
+end
+
+--
+-- LISTED
+--
 
 function Buf:is_listed()
 	return vim.fn.buflisted(self.bufnr)
 end
 
+--- Sets this buffer to be listed or unlisted; defaults to listed if a value is not provided.
 --- @param bool boolean?
 function Buf:set_listed(bool)
 	if bool == nil then bool = true end
@@ -88,8 +163,19 @@ function Buf:set_listed(bool)
 	return self
 end
 
+--- Sets this buffer to be unlisted.
 function Buf:set_unlisted()
 	return self:set_listed(false)
+end
+
+--
+-- WINDOWS
+--
+
+--- Returns a List of window-IDs for windows that contain this buffer.  When
+--- there is none the list is empty.
+function Buf:get_window_numbers()
+	return vim.fn.win_findbuf(self.bufnr)
 end
 
 
@@ -124,24 +210,10 @@ function Buf.util.find_by_name(name)
 	return nil
 end
 
--- local buf = Buf.get(1)
--- vim.notify(vim.inspect(buf.get_lines))
-
--- local lines = {}
--- for _, bufnr in pairs(Buf.list()) do
--- 	local b = Buf.get(bufnr)
--- 	local loaded = b:is_loaded() and "(loaded)" or "(unloaded)"
--- 	local line = bufnr .. ": " .. b:get_name() .. " " .. loaded
-
--- 	table.insert(lines, line)
--- end
-
--- vim.notify(vim.fn.join(lines, "\n"))
-
 
 -- Buffer Functions                                                  *api-buffer*
 --
--- nvim_create_buf({listed}, {scratch})
+-- x	-- nvim_create_buf({listed}, {scratch})
 -- nvim_list_bufs()
 --
 -- nvim_buf_attach({buffer}, {send_buffer}, {opts})
@@ -157,22 +229,22 @@ end
 --
 -- nvim_buf_get_changedtick({buffer})
 -- nvim_buf_get_keymap({buffer}, {mode})
--- nvim_buf_get_lines({buffer}, {start}, {end}, {strict_indexing})
+-- x	-- nvim_buf_get_lines({buffer}, {start}, {end}, {strict_indexing})
 -- nvim_buf_get_mark({buffer}, {name})
 -- nvim_buf_get_name({buffer})
 -- nvim_buf_get_offset({buffer}, {index})
 -- nvim_buf_get_text({buffer}, {start_row}, {start_col}, {end_row}, {end_col}, ...
 -- nvim_buf_get_var({buffer}, {name})
 --
--- nvim_buf_is_loaded({buffer})
+-- x	-- nvim_buf_is_loaded({buffer})
 -- nvim_buf_is_valid({buffer})
 --
--- nvim_buf_line_count({buffer})
+-- x	-- nvim_buf_line_count({buffer})
 --
 -- nvim_buf_set_keymap({buffer}, {mode}, {lhs}, {rhs}, {*opts})
--- nvim_buf_set_lines({buffer}, {start}, {end}, {strict_indexing}, {replacement})
+-- x	-- nvim_buf_set_lines({buffer}, {start}, {end}, {strict_indexing}, {replacement})
 -- nvim_buf_set_mark({buffer}, {name}, {line}, {col}, {opts})
--- nvim_buf_set_name({buffer}, {name})
+-- x	-- nvim_buf_set_name({buffer}, {name})
 -- nvim_buf_set_text({buffer}, {start_row}, {start_col}, {end_row}, {end_col}, ...
 -- nvim_buf_set_var({buffer}, {name}, {value})
 
